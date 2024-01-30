78c8a94d2f69ed567f9c3e2b5dc8438e50fff515
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2024-01-30 21:30:00 +0530

    updated config

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ os.environ["GOOGLE_API_KEY"] = "AIzaSyB_Wf8VWRPNMp6KTaR_0T0rxEmAEyObEkU"
 
 app = App.from_config(config_path="config.yaml")
 
-app.add("https://www.forbes.com/profile/elon-musk")
+app.add(r"C:\Users\Rupesh\Downloads\test.pdf", data_type="pdf_file")
 
-response = app.query("create 3 quizz questions with 4 choices and correct answer")
+response = app.query(
+    "create 2 quizz questions with 4 choices and correct answer about Adapter Pattern"
+)
+# response = app.query("create a list of topics")
 if app.llm.config.stream:  # if stream is enabled, response is a generator
     for chunk in response:
         print(chunk)
